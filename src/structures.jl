3e3c4a5ba1951b6158d74c07f9aefb7da42cf951
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-04-07 23:40:35 +0000

    Snapshot of branch master taken on 2019-04-07T19:40:34.464-04:00 from "https://github.com/UnofficialJuliaMirror/LikelihoodProfiler.jl-93acb638-a083-5915-8dce-d129bc6a3f59"

---
+++ src/structures.jl
@@ -0,0 +1,36 @@
+"""
+    struct ProfilePoint
+        value::Float64
+        loss::Float64
+        params::Array{Float64, 1}
+        ret::Symbol
+        counter::Union{Int, Nothing}
+    end
+
+Structure storing one point from profile function.
+"""
+struct ProfilePoint
+    value::Float64
+    loss::Float64
+    params::Array{Float64, 1}
+    ret::Symbol
+    counter::Union{Int, Nothing}
+end
+
+"""
+    struct EndPoint
+        value::Float64
+        profilePoints::Array{ProfilePoint, 1}
+        status::Symbol
+        direction::Symbol
+        counter::Int
+    end
+Structure storing end point for confidence interval.
+"""
+struct EndPoint
+    value::Union{Float64, Nothing}
+    profilePoints::Array{ProfilePoint, 1}
+    status::Symbol
+    direction::Symbol
+    counter::Int
+end
